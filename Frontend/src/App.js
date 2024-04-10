71f4349db29b2a7961f489cad374627f17c69be5
Amit Kumar <amitk.agilsys@gmail.com>
2024-04-10 09:54:13 +0530

    conn str

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,10 +4,12 @@ import UserForm from './Component/UserForm/UserForm.jsx';
 import UserList from './Component/UserData/UserList';
 import Login from './Component/login/login.jsx';
 import Signup from './Component/signup/Signup.jsx';
-// import axios from "axios";
+import axios from "axios";
 
 function App() {
-  // axios.interceptors.request.use
+  axios.interceptors.request.use((request) => {
+    console.log(request);
+  })
   return (
     <Router>
       <div className="app">
@@ -15,7 +17,7 @@ function App() {
           <Routes>
             <Route path="/userList" element={<UserList />} />
             <Route path="/userForm" element={<UserForm />} />
-            <Route path="/login" element={<Login />} /> 
+            <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} /> .
             <Route path="/userForm/:id" element={<UserForm />} />
             <Route path="/" element={<UserForm />} />

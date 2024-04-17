e2cac52bc3474eb0e4f419f703d9fe6a45bb5003
Mrityunjai2707 <vishushukla9919@gmail.com>
2024-04-17 14:05:13 +0530

    Structure Modify

--- Backend/controllers/getUserRoleController.js
+++ Backend/controllers/getUserRoleController.js
@@ -3,7 +3,6 @@ const { Role } = require("../models/userRoleModel");
 const getUserRole = async (req, res) => {
   try {
     const roles = await Role.find({});
-    console.log("getUserRole", roles);
     res.status(200).json(roles);
   } catch (error) {
     console.log("getUserRole");

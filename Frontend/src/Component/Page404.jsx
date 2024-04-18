9e14b6c405cff4f4e2e80cf7871ea9c7bd61b147
Amit Kumar <amitk.agilsys@gmail.com>
2024-04-18 18:21:02 +0530

    changes

--- Frontend/src/Component/Page404.jsx
+++ Frontend/src/Component/Page404.jsx
@@ -6,7 +6,8 @@ function Page404() {
     <div className="container-fluid p-0 position-relative">
       <iframe className='w-100 vh-100' src="https://lottie.host/embed/af6d9e62-3687-4fe8-a0f7-38df5bee86d7/rYEcru3gN6.json"></iframe>
       <div className="d-flex justify-content-between align-items-end">
-        <Link to="/" className='position-absolute top-50 start-50 translate-middle fs-1 text-white text-decoration-none px-4 py-2 border border-light rounded-2'>Go Back</Link>
+        <Link to="/" className='position-absolute top-50 start-50 translate-middle fs-1 text-white text-decoration-none px-4 py-2 border border-light rounded-2'>Go Back
+        </Link>
       </div>
     </div>
   )
